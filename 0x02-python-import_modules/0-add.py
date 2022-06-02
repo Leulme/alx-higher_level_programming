e72591ba6168656a59e74af107337310f4754dae
Leulme <kebedemichael9@gmail.com>
2022-06-02 18:12:54 +0300

    Added import module project

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-# 0_add.py
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
 if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
-
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    main()
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-# 3-infinite_add.py
+import sys
+
+
+def main(*argv):
+    l = len(sys.argv)
+    sum = 0
+    if l > 1:
+        for args in sys.argv:
+            if args != sys.argv[0]:
+                sum = sum + int(args)
+    print(sum)
 
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    main()
